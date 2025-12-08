9c5c065b52ba31f5f99858541dec81ff5c02ce7d
Isaias morais <isaiassenac10@gmail.com>
2025-12-08 18:53:02 -0300

    refactorando

--- modelos/moto.py
+++ modelos/moto.py
@@ -1,16 +1,18 @@
 from datetime import datetime
+from multiprocessing.spawn import prepare
+
 
 class Moto:
 
     def __init__(self,modelo,cor,quilometragem = 0):
-        self.modelo = modelo
-        self.cor = cor
-        self.quilometrgem = quilometragem
-        self.abastecimento = []
-        self.manutencoes = []
+        self._modelo = modelo
+        self._cor = cor
+        self._quilometrgem = quilometragem
+        self._abastecimento = []
+        self._manutencoes = []
 
     def __str__(self):
-        return f"{self.modelo},{self.cor}"
+        return f"{self._modelo},{self._cor}"
 
     def RegistraAbastecimento(self,valor,litros,completo = False,  data = None):
         if not isinstance(valor,(int ,float)) or valor <0:
@@ -23,19 +25,16 @@ class Moto:
         if not isinstance(dia,(datetime)):
             return f'Digite uma data valida'
         else:
-            valorRS = valor
-            litrosL = litros
-            tanque_completo = completo
             tanque_completo_str = 'Sim' if tanque_completo else 'Não'
-            PrecoLitro = valor / litros
+            preco_litro = valor / litros
             registor ={
                 'Dia':dia,
-                'Valor':valorRS,
-                'Litros':litrosL,
-                'Preço_Do_litro':PrecoLitro,
+                'Valor':valor,
+                'Litros':litros,
+                'Preço_Do_litro':preco_litro,
                 'Completou':tanque_completo_str
             }
-            self.abastecimento.append(registor)
+            self._abastecimento.append(registor)
 
     def RegistraManutencao(self,peca='',valor_peca=0,quilometrgem_de_troca=None, data = None):
 
@@ -59,7 +58,7 @@ class Moto:
                 'Valor':valor,
                 'Quilometragem_de_troca':QuilometrgemDeTroca
             }
-            self.manutencoes.append(registro)
+            self._manutencoes.append(registro)
 
 
 
