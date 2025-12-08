25a852817cfdba5256ac8198fdde1350f5f5b11e
Isaias morais <isaiassenac10@gmail.com>
2025-12-08 00:11:45 -0300

    primeiro teste

--- modelos/moto.py
+++ modelos/moto.py
@@ -13,16 +13,16 @@ class Moto:
         return f"{self.modelo},{self.cor}"
 
     def RegistraAbastecimento(self,valor,litros,completo = False,  data = None):
-        if not isinstance(valor,int ,float) or valor <0:
+        if not isinstance(valor,(int ,float)) or valor <0:
             return f'Digite um valor valido'
-        if not isinstance(litros,int ,float) or valor <0:
+        if not isinstance(litros,(int ,float)) or valor <0:
             return f'Digite um litragem valida'
-        if not isinstance(completo,bool):
+        if not isinstance(completo,(bool)):
             return f'Digite um valor bool valido'
-        if not isinstance(data,datetime) or data == None:
+        dia = data or datetime.today()
+        if not isinstance(dia,(datetime)):
             return f'Digite uma data valida'
         else:
-            dia = data or datetime.today()
             valorRS = valor
             litrosL = litros
             tanque_completo = completo
@@ -39,16 +39,16 @@ class Moto:
 
     def RegistraManutencao(self,peca='',valor_peca=0,quilometrgem_de_troca=None, data = None):
 
-        if not isinstance(valor_peca, int , float):
+        if not isinstance(valor_peca, (int , float)):
             return 'Digite um valor valido'
 
-        if not isinstance(quilometrgem_de_troca,int,float):
+        if not isinstance(quilometrgem_de_troca,(int,float)):
             return  'Digite uma quilometragem de troca '
-
-        if not isinstance(data,datetime) or data == None:
+        dia = data or datetime.today()
+        if not isinstance(dia,(datetime)):
             return 'Digite uma data valida'
         else:
-            dia = data
+
             pecaStr = peca
             valor = valor_peca
             QuilometrgemDeTroca = quilometrgem_de_troca
--- teste.py
+++ teste.py
@@ -1,2 +1,6 @@
 from modelos.moto import Moto
 moto1 = Moto('Cg 160',"preta",0)
+moto1.RegistraManutencao('oleo',40,1000)
+moto1.RegistraAbastecimento(60,10,True)
+print(moto1.manutencoes)
+print(moto1.abastecimento)
